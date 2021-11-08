75aea31fcf8c7efe5e0acd0ce9ac75204ac5af76
David Greven <fixed-term.David.Greven@de.bosch.com>
2021-11-08 14:29:12 +0100

    Add missing file header
    Signed-off-by: David Greven <fixed-term.David.Greven@de.bosch.com>

--- common-types/src/test/java/io/carbynestack/common/result/ResultTest.java
+++ common-types/src/test/java/io/carbynestack/common/result/ResultTest.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2021 - for information on the respective copyright owner
+ * see the NOTICE file and/or the repository https://github.com/carbynestack/common.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package io.carbynestack.common.result;
 
 import org.junit.jupiter.api.Test;
