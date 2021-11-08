9c9b969be8c27c1ec2b109c6e12085d2c0b53e86
David Greven <fixed-term.David.Greven@de.bosch.com>
2021-11-08 10:18:14 +0100

    Update cs-common-testing test style
    Signed-off-by: David Greven <fixed-term.David.Greven@de.bosch.com>

--- common-testing/src/test/java/io/carbynestack/testing/result/ResultAssertTest.java
+++ common-testing/src/test/java/io/carbynestack/testing/result/ResultAssertTest.java
@@ -15,16 +15,16 @@ import org.opentest4j.AssertionFailedError;
 import static io.carbynestack.testing.result.ResultAssert.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class ResultAssertTest {
-    public final int value = 12;
-    public final int reason = 21;
-    public final String some = "some";
-    public final String none = "none";
-    public final Result<Integer, Integer> success = new Success<>(value);
-    public final Result<Integer, Integer> failure = new Failure<>(reason);
+class ResultAssertTest {
+    private final int value = 12;
+    private final int reason = 21;
+    private final String some = "some";
+    private final String none = "none";
+    private final Result<Integer, Integer> success = new Success<>(value);
+    private final Result<Integer, Integer> failure = new Failure<>(reason);
 
     @Test
-    public void isSuccess() {
+    void isSuccess() {
         assertThat(success).isSuccess();
         assertThatThrownBy(() -> assertThat(failure).isSuccess())
                 .isExactlyInstanceOf(AssertionError.class)
@@ -32,7 +32,7 @@ public class ResultAssertTest {
     }
 
     @Test
-    public void isFailure() {
+    void isFailure() {
         assertThat(failure).isFailure();
         assertThatThrownBy(() -> assertThat(success).isFailure())
                 .isExactlyInstanceOf(AssertionError.class)
@@ -40,7 +40,7 @@ public class ResultAssertTest {
     }
 
     @Test
-    public void hasValue() {
+    void hasValue() {
         assertThat(success).hasValue(value);
         assertThatThrownBy(() -> assertThat(failure).hasValue(value))
                 .isExactlyInstanceOf(AssertionError.class)
@@ -51,28 +51,28 @@ public class ResultAssertTest {
     }
 
     @Test
-    public void hasStringValue() {
+    void hasStringValue() {
         assertThatThrownBy(() -> assertThat(new Success<>(some)).hasValue(none))
                 .isExactlyInstanceOf(AssertionFailedError.class)
                 .hasMessage("Expecting result success value to equal '%s' but was: '%s'", none, some);
     }
 
     @Test
-    public void hasStringValueWithSingleQuotes() {
+    void hasStringValueWithSingleQuotes() {
         assertThatThrownBy(() -> assertThat(new Success<>("'first'")).hasValue("second"))
                 .isExactlyInstanceOf(AssertionFailedError.class)
                 .hasMessage("Expecting result success value to equal 'second' but was: '%s'", "'first'");
     }
 
     @Test
-    public void hasStringValueWithLineBreaks() {
+    void hasStringValueWithLineBreaks() {
         assertThatThrownBy(() -> assertThat(new Success<>("first\nsecond")).hasValue("third"))
                 .isExactlyInstanceOf(AssertionFailedError.class)
                 .hasMessage("Expecting result success value to equal 'third' but was: '%s'", "first\\nsecond");
     }
 
     @Test
-    public void hasReason() {
+    void hasReason() {
         assertThat(failure).hasReason(reason);
         assertThatThrownBy(() -> assertThat(success).hasReason(reason))
                 .isExactlyInstanceOf(AssertionError.class)
@@ -83,21 +83,21 @@ public class ResultAssertTest {
     }
 
     @Test
-    public void hasStringReason() {
+    void hasStringReason() {
         assertThatThrownBy(() -> assertThat(new Failure<>(some)).hasReason(none))
                 .isExactlyInstanceOf(AssertionFailedError.class)
                 .hasMessage("Expecting result failure reason to equal '%s' but was: '%s'", none, some);
     }
 
     @Test
-    public void hasStringReasonWithSingleQuotes() {
+    void hasStringReasonWithSingleQuotes() {
         assertThatThrownBy(() -> assertThat(new Failure<>("'first'")).hasReason("second"))
                 .isExactlyInstanceOf(AssertionFailedError.class)
                 .hasMessage("Expecting result failure reason to equal 'second' but was: '%s'", "'first'");
     }
 
     @Test
-    public void hasStringReasonWithLineBreaks() {
+    void hasStringReasonWithLineBreaks() {
         assertThatThrownBy(() -> assertThat(new Failure<>("first\nsecond")).hasReason("third"))
                 .isExactlyInstanceOf(AssertionFailedError.class)
                 .hasMessage("Expecting result failure reason to equal 'third' but was: '%s'", "first\\nsecond");
